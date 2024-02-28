04cf76e2348696c3af8f8f2a47900a96df1004e7
tbohn2 <tanner.bohn@yahoo.com>
2024-02-28 09:35:56 -0700

    Added home, contact and header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
-
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Header from './components/Header'
 import './styles/App.css'
 
 function App() {
 
-
   return (
-    <>
-
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import '../styles/App.css'
+
+const Header = () => {
+    return (
+        <header>
+            <h1 className="annapurna-sil">Willy Nilly Bus</h1>
+        </header>
+    );
+}
+
+export default Header;
