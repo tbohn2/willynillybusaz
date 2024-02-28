bb588714a0418cee94b96e1c26ecf0901e2274ef
tbohn2 <tanner.bohn@yahoo.com>
2024-02-28 15:26:14 -0700

    Made header; added CSS classes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import '../styles/App.css'
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleMenuClick = () => {
+        setMenuOpen(!menuOpen);
+    }
+
+    useEffect(() => {
+        const closeMenu = () => {
+            if (menuOpen) {
+                setMenuOpen(false);
+            }
+        };
+
+        document.body.addEventListener("click", closeMenu);
+
+        return () => {
+            document.body.removeEventListener("click", closeMenu);
+        };
+    }, [menuOpen]);
+
+    const handleMenuContainerClick = (e) => {
+        e.stopPropagation();
+    };
+
     return (
-        <header>
-            <h1 className="annapurna-sil">Willy Nilly Bus</h1>
+        <header className="annapurna-sil-bold d-flex justify-content-evenly align-items-center">
+            <h1 className="">WILLY NILLY BUS AZ</h1>
+            <div className="menu-container" onClick={handleMenuContainerClick}>
+                <button type="button" className="menu-icon text-center" onClick={handleMenuClick}>&#9776;</button>
+                <ul className={menuOpen ? 'menu display-block' : 'hidden'}>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+            </div>
         </header>
     );
 }
